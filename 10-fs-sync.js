e6dad0a0be25ae3f4f40441a4286605374519084
Kingsley <kifeanyi216@gmail.com>
2025-02-19 13:08:46 +0100

    Async patterns

--- 10-fs-sync.js
+++ 10-fs-sync.js
@@ -1,11 +1,33 @@
-const {readFileSync, writeFileSync} = require('fs');
-console.log('start');
+// const {readFileSync, writeFileSync} = require('fs');
+// console.log('start');
 
-const first = readFileSync('./content/first.txt', 'utf8')
-const second = readFileSync('./content/second.txt', 'utf8')
+// const first = readFileSync('./content/first.txt', 'utf8')
+// const second = readFileSync('./content/second.txt', 'utf8')
 
-console.log(first, second);
+// console.log(first, second);
 
-writeFileSync('./content/result-sync.txt', `Here is the result: ${first}, ${second}`, {flag: 'a'});
-console.log('done with this task');
-console.log('starting the next one');
+// writeFileSync('./content/result-sync.txt', `Here is the result: ${first}, ${second}`, {flag: 'a'});
+// console.log('done with this task');
+// console.log('starting the next one');
+
+
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    if(req.url === '/') {
+        res.end('Home Page')
+    }
+    if(req.url === '/about') {
+        // blocking code
+        for(let i = 0; i < 1000; i++) {
+            for(let j = 0; j < 1000; j++) {
+             console.log(`${i} ${j}`)   
+            }
+        }
+        res.end('About Page')
+    }
+})
+
+server.listen(5000, () => {
+    console.log('Server listening on port : 5000...')
+})
--- app.js
+++ app.js
@@ -12,9 +12,72 @@
 // npm init (step by step, press enter to skip)
 // npm init -y (everything default)
 
-const _ = require('lodash');
+// const _ = require('lodash');
 
-const items = [1, [2, [3, [4]]]];
-const newItems = _.flattenDeep(items);
-console.log(newItems);
-console.log('Hello World!');
+// const items = [1, [2, [3, [4]]]];
+// const newItems = _.flattenDeep(items);
+// console.log(newItems);
+// console.log('Hello World!');
+
+// const {readFile} = require('fs');
+// console.log('started a first task')
+
+// readFile('./content/first.txt', 'utf8', (err, result) => {
+//     if(err) {
+//         console.log(err)
+//         return
+//     }
+//     console.log(result)
+//     console.log('Completed first task')
+// })
+// console.log('starting next task')
+
+// started operating system process
+// console.log('first')
+// setTimeout(() => {
+//     console.log('second')
+// }, 0)
+// console.log('third')
+// completed and exited operating system process
+
+// setInterval(() => {
+//     console.log('hello world')
+// }, 2000)
+// console.log('I will run first')
+
+const {readFile, writeFile} = require('fs').promises;
+// const util = require('util');
+
+// const readFilePromise = util.promisify(readFile);
+// const writeFilePromise = util.promisify(writeFile);
+
+
+const start = async() => {
+    try {
+        const first = await readFile('./content/first.txt', 'utf8')
+        const second = await readFile('./content/second.txt', 'utf8')
+
+        await writeFile('./content/result-mind-grenade.txt', `This is Awesome: ${first} ${second}`, {flag: 'a'})
+        console.log(first, second)
+    } catch (error) {
+        console.log(error)
+        
+    }
+}
+start()
+
+
+// const getText = (path) => {
+//     return new Promise((resolve, reject) => {
+//      readFile(path, 'utf8', (err, data) => {
+//     if(err) {
+//         reject(err);
+//     } else {
+//         resolve(data);
+//     }
+// })
+//     })
+// }
+
+// getText('./content/first.txt').then((result) => console.log(result)).catch((err) => console.log(err))
+
